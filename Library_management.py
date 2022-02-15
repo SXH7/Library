b7864cce1e92430fa4477bcdb2aef9b762e8a64a
Pratyush <karmasama123456@gmail.com>
2022-02-16 00:34:33 +0530

    commit test

--- Library_management.py
+++ Library_management.py
@@ -1,1 +1,14 @@
-print("hello")
+from pydoc import importfile
+import mysql.connector as i
+g=open("password.txt").read
+log=i.connect(host="localhost",user="root",password=g,database="library")
+
+def addbook():
+    bn=input("Enter the book name:")
+    c=input("Enter the book code:")
+    t=input("Enter the total number of books:")
+    s=input("Enter the subject of books:")
+    data = (bn,c,t,s)
+    sql='insert into book values(%s,%s,%s,%s)'
+    c=log.cursor()
+    c.execute
