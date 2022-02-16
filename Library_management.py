3654e3942c40d59047b2fc4a856c4e3bbd0f9018
Pratyush <karmasama123456@gmail.com>
2022-02-16 09:09:29 +0530

    commit 3

--- Library_management.py
+++ Library_management.py
@@ -20,3 +20,33 @@ def addbook():
     print("Book(s) Added sucessfully")
     print("------------------------------------------------------------------")
 
+def issueb():
+    ina=input("Enter name:")
+    reg=input("Enter registration no.:")
+    bco=input("Enter book code:")
+    isu=input("Enter Date:")
+    data=(ina,reg,bco,isu)
+    sql='insert into issue values(%s,%s,%s,%s)'
+    c=log.cursor()
+    c.execute(sql,data)
+    log.commit()
+    print("Issue done sucessfully")
+    print("------------------------------------------------------------------")
+    bookup=(bco,-1)
+
+def summitb():
+    ina=input("Enter name:")
+    reg=input("Enter registration no.:")
+    bco=input("Enter book code:")
+    isu=input("Enter Date:")
+    data=(ina,reg,bco,isu)
+    sql='insert into summit values(%s,%s,%s,%s)'
+    c=log.cursor()
+    c.execute(sql,data)
+    log.commit()
+    print("Submission done sucessfully")
+    print("------------------------------------------------------------------")
+    bookup=(bco,+1)
+
+
+
